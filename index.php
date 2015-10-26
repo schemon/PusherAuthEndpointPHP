84a1ec63d74d0e6efda153b62fe1bd94a3007c66
root <root@ip-172-31-26-124.eu-central-1.compute.internal>
2015-10-26 21:49:43 +0000

    its ok

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php 
 require __DIR__ . '/vendor/autoload.php';
 
-$app_id = 'YOUR_APP_ID';
-$app_key = 'YOUR_APP_KEY';
-$app_secret = 'YOUR_APP_SECRET';
+
+
+$app_id = '134441';
+$app_key = '599cb5ed77cd5efb659a';
+$app_secret = '76a57ea82e311bcbbb1f';
+
+error_log('post ' . var_export($_SERVER, true));
+
+$channel_name = $_POST['channel_name'];
+$socket_id = $_POST['socket_id'];
+
+error_log($app_id);
+error_log($app_key);
+error_log($app_secret);
+error_log('channel name: ' . $channel_name);
+error_log('socket id: ' . $socket_id);
+
 
 $pusher = new Pusher( $app_key, $app_secret, $app_id );
-echo $pusher->socket_auth('private-rpi', '74053.4303202');
-
+echo $pusher->socket_auth($channel_name, $socket_id);
